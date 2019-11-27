c782fb6e5b7ecace856aec162689d8ad8d4b251c
wchxiong <wchxiong@126.com>
2019-11-27 10:19:37 +0800

    change install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,46 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt-get update
 
-sudo apt-get install docker-ce
+echo '123456' | sudo -S apt-get install -y \
+     docker-ce
 
-sudo apt-get install docker-compose
+#sudo apt-get install docker-compose
 
 echo "Install AutoRS"
 
-sudo git clone https://github.com/a2008301610258/AutoRS_Docker.git
+git clone https://github.com/a2008301610258/AutoRS_Docker.git
 
-sudo git checkout -b csg_v1 
+cd ./AutoRS_Docker
+
+git checkout -b csg remotes/origin/csg
+
+mkdir /home/zhihui/AutoRS
+
+cd ..
+
+cp -r ./AutoRS_Docker/* /home/zhihui/AutoRS/
+
+cd /home/zhihui/AutoRS
+
+bash init.sh
+
+echo "Install AutoRS Docker"
+
+echo '123456' | sudo -S apt-get install -y \
+      git
+
+echo 'a2008301610258' | sudo docker login --username=wchxiong registry.cn-shenzhen.aliyuncs.com
+
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/autors/csg:v1
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chmod 777 /etc/rc.local
+
+sudo echo -e "#!/bin/sh -e\n" > /etc/rc.local
+
+sudo echo -e "/usr/local/bin/docker-compose -f /home/zhihui/AutoRS/docker-compose.yml up -d\n" >> /etc/rc.local
+
+sudo echo -e "exit 0" >> /etc/rc.local
